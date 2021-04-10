380475526a6d5acfd5c9e4199840a9ecc5cc054a
Tsuki Akiba <erase2004@gmail.com>
2021-04-10 11:10:32 +0800

    1. fixed testcase display issue

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -16,36 +16,36 @@ function containsDuplicate(nums) {
 let result, n, ans;
 n = [];
 ans = 'boolean';
-console.log('containsDuplicate(%s) should return %s:', n, ans);
+console.log('containsDuplicate(%s) should return %s:', JSON.stringify(n), ans);
 result = (typeof containsDuplicate(n) === ans);
 console.log('%c %s', `color : ${result? 'green' : 'red'}`, `${result? 'pass' : 'failed'}`);
 
 n = [1];
 ans = false;
-console.log('containsDuplicate(%s) should return %s:', n, ans);
+console.log('containsDuplicate(%s) should return %s:', JSON.stringify(n), ans);
 result = (containsDuplicate(n) === ans);
 console.log('%c %s', `color : ${result? 'green' : 'red'}`, `${result? 'pass' : 'failed'}`);
 
 n = [1,1];
 ans = true;
-console.log('containsDuplicate(%s) should return %s:', n, ans);
+console.log('containsDuplicate(%s) should return %s:', JSON.stringify(n), ans);
 result = (containsDuplicate(n) === ans);
 console.log('%c %s', `color : ${result? 'green' : 'red'}`, `${result? 'pass' : 'failed'}`);
 
 n = [1,2];
 ans = false;
-console.log('containsDuplicate(%s) should return %s:', n, ans);
+console.log('containsDuplicate(%s) should return %s:', JSON.stringify(n), ans);
 result = (containsDuplicate(n) === ans);
 console.log('%c %s', `color : ${result? 'green' : 'red'}`, `${result? 'pass' : 'failed'}`);
 
 n = [1,2,3];
 ans = false;
-console.log('containsDuplicate(%s) should return %s:', n, ans);
+console.log('containsDuplicate(%s) should return %s:', JSON.stringify(n), ans);
 result = (containsDuplicate(n) === ans);
 console.log('%c %s', `color : ${result? 'green' : 'red'}`, `${result? 'pass' : 'failed'}`);
 
 n = [2,4,6, 1, 74, 36, 6];
 ans = true;
-console.log('containsDuplicate(%s) should return %s:', n, ans);
+console.log('containsDuplicate(%s) should return %s:', JSON.stringify(n), ans);
 result = (containsDuplicate(n) === ans);
 console.log('%c %s', `color : ${result? 'green' : 'red'}`, `${result? 'pass' : 'failed'}`);
